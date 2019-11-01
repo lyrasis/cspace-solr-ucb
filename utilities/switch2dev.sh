91e84b6a3b0308567d026f30024cdecd50d2ac2a
John Lowe <jblowe@berkeley.edu>
2019-10-31 17:20:23 -0700

    CSW-122: helper scripts for -dev and -qa solr deployments

--- utilities/switch2dev.sh
+++ utilities/switch2dev.sh
@@ -2,8 +2,8 @@
 # a lil script to change the solr pipelines to point to qa instead of prod and to
 # send email notifications to jblowe and not the various "real" addresses. 
 perl -i -pe 's/prod.42.ist.berkeley.edu port=53/dev-42.ist.berkeley.edu port=53/' solrdatasources/*/*.sh
-perl -i -pe 's/=5313/=5113/' solrdatasources/*/*.sh
-perl -i -pe 's/=5307/=5107/' solrdatasources/*/*.sh
-perl -i -pe 's/=5310/=5110/' solrdatasources/*/*.sh
+perl -i -pe 's/=5313/=5114/' solrdatasources/*/*.sh
+perl -i -pe 's/=5307/=5117/' solrdatasources/*/*.sh
+perl -i -pe 's/=5310/=5119/' solrdatasources/*/*.sh
 perl -i -pe 's/cspace.support.lists./jblowe@/' solrdatasources/*/*.sh
 perl -i -pe 's/CONTACT=.*/CONTACT="jblowe\@berkeley.edu"/' solrdatasources/*/*.sh
