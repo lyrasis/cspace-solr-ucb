95e82f882237d8d12b6f1d58357093a3be279068
John Lowe <jblowe@berkeley.edu>
2020-05-06 14:47:01 -0700

    CSW-310: minor improvements to pahma 'museum number search verification script'

--- utilities/verify_pahma_objnos.sh
+++ utilities/verify_pahma_objnos.sh
@@ -1,3 +1,11 @@
+#!/usr/bin/env bash
+
+# single argument: a list of PAHMA museum numbers, one per line
+if [ $# -ne 1 ]; then
+    echo "usage: $0 list_of_museum_numbers_to_check.txt"
+    exit
+fi
+
 
 python tS.py pahma-public http://localhost:8983 'objmusno_txt:"%s"' < $1 > objmusno_txt.txt &
 python tS.py pahma-public http://localhost:8983 'objmusno_s:"%s"' < $1 > objmusno_s.txt &
@@ -6,9 +14,16 @@ perl -pe 'tr/A-Z/a-z/' $1 | python tS.py pahma-public http://localhost:8983 'obj
 
 wait
 
+echo "frequency distribution of queries and number of results"
+echo
+echo "e.g. if there are 100 queries, and all return a single result (which should be the case for museum numbers)"
+echo "the output, at least for the 'lower case' result, should be '100   1'."
+echo
 for x in s_lower s_vs_lower s txt
 do
     echo
     echo "objmusno_${x}.txt"
-    cut -f2 objmusno_${x}.txt | sort -n | uniq -c | head -20
+    echo "number of queries  number results"
+    echo "=================  =============="
+    cut -f2 objmusno_${x}.txt | sort -n | uniq -c | head -20 | perl -pe 's/(\d+) (\d+)/\1                 \2/'
 done
