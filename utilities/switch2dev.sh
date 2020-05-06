7e9064be49d3fc0c96b7c36a23419f886120b257
John Lowe <jblowe@berkeley.edu>
2020-05-06 14:43:55 -0700

    CSW-310: ensure that cinefiles 'nightly denorm' is handled in Dev and QA solr etl config

--- utilities/switch2dev.sh
+++ utilities/switch2dev.sh
@@ -1,7 +1,9 @@
+#!/usr/bin/env bash
 #
-# a lil script to change the solr pipelines to point to qa instead of prod and to
-# send email notifications to jblowe and not the various "real" addresses. 
+# a lil script to change the solr pipelines to point to dev instead of prod and to
+# send email notifications to jblowe and not the various "real" addresses.
 perl -i -pe 's/prod-45.ist.berkeley.edu port=53/dev-45.ist.berkeley.edu port=53/' solrdatasources/*/*.sh
+perl -i -pe 's/prod-45.ist.berkeley.edu port=53/dev-45.ist.berkeley.edu port=53/' solrdatasources/cinefiles/scripts/*.sh
 perl -i -pe 's/=5313/=5114/' solrdatasources/*/*.sh
 perl -i -pe 's/=5307/=5117/' solrdatasources/*/*.sh
 perl -i -pe 's/=5310/=5119/' solrdatasources/*/*.sh
--- utilities/switch2qa.sh
+++ utilities/switch2qa.sh
@@ -1,7 +1,9 @@
+#!/usr/bin/env bash
 #
 # a lil script to change the solr pipelines to point to qa instead of prod and to
 # send email notifications to jblowe and not the various "real" addresses. 
 perl -i -pe 's/prod-45.ist.berkeley.edu port=53/dev-45.ist.berkeley.edu port=53/' solrdatasources/*/*.sh
+perl -i -pe 's/prod-45.ist.berkeley.edu port=53/dev-45.ist.berkeley.edu port=53/' solrdatasources/cinefiles/scripts/*.sh
 perl -i -pe 's/=5313/=5113/' solrdatasources/*/*.sh
 perl -i -pe 's/=5307/=5107/' solrdatasources/*/*.sh
 perl -i -pe 's/=5310/=5110/' solrdatasources/*/*.sh
