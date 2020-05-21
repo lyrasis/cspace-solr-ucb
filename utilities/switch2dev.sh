91c04dccba86af71521d293b291b97c11e4738c9
John Lowe <jblowe@berkeley.edu>
2020-05-21 08:52:01 -0700

    CSW-321: switch2*.sh should run correctly from anywhere

--- utilities/switch2dev.sh
+++ utilities/switch2dev.sh
@@ -2,7 +2,8 @@
 #
 # a lil script to change the solr pipelines to point to dev instead of prod and to
 # send email notifications to jblowe and not the various "real" addresses.
-perl -i -pe 's/prod-42.ist.berkeley.edu port=53/dev-45.ist.berkeley.edu port=53/' solrdatasources/*/*.sh
+cd
+perl -i -pe 's/prod-42.ist.berkeley.edu/dev-45.ist.berkeley.edu/' solrdatasources/*/*.sh
 perl -i -pe 's/=5313/=5114/' solrdatasources/*/*.sh
 perl -i -pe 's/=5307/=5117/' solrdatasources/*/*.sh
 perl -i -pe 's/=5310/=5119/' solrdatasources/*/*.sh
--- utilities/switch2qa.sh
+++ utilities/switch2qa.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 #
 # a lil script to change the solr pipelines to point to qa instead of prod and to
-# send email notifications to jblowe and not the various "real" addresses. 
-perl -i -pe 's/prod-42.ist.berkeley.edu port=53/dev-45.ist.berkeley.edu port=53/' solrdatasources/*/*.sh
+# send email notifications to jblowe and not the various "real" addresses.
+cd
+perl -i -pe 's/prod-42.ist.berkeley.edu/dev-45.ist.berkeley.edu/' solrdatasources/*/*.sh
 perl -i -pe 's/=5313/=5113/' solrdatasources/*/*.sh
 perl -i -pe 's/=5307/=5107/' solrdatasources/*/*.sh
 perl -i -pe 's/=5310/=5110/' solrdatasources/*/*.sh
