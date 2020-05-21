be495631a822bd3f71e9dfde11a20f9c5bf4ba8d
John Lowe <jblowe@berkeley.edu>
2020-05-20 17:58:34 -0700

    CSW-321: add timestamps to Solr ETL pipeline logs

--- utilities/oj.cinefiles.sh
+++ utilities/oj.cinefiles.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # run the 'legacy denormalizing' script first, to populate the 'cinefiles_denorm' db schema used by the portal
-cd /home/app_solr/solrdatasources/cinefiles/scripts/ ; /bin/bash -l -c './cinefiles_denorm_nightly.sh' ; cd
+cd /home/app_solr/solrdatasources/cinefiles ; /bin/bash -l -c './cinefiles_denorm_nightly.sh' ; cd
 # now run the regular pipeline that populates solr
-/home/app_solr/solrdatasources/cinefiles/solrETL-public.sh        cinefiles >> /home/app_solr/logs/cinefiles.solr_extract_public.log  2>&1
+/home/app_solr/solrdatasources/cinefiles/solrETL-public.sh        cinefiles 2>&1 | /usr/bin/ts '[%Y-%m-%d %H:%M:%S]' >> /home/app_solr/logs/cinefiles.solr_extract_public.log  2>&1
