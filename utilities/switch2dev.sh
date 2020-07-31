094a03830df1e1ecf7b91db39fe58da22f245f7e
John Lowe <jblowe@berkeley.edu>
2020-07-31 11:16:05 -0700

    CSW-352: update 'config fixer' scripts for -dev and -qa solr pipelines

--- utilities/switch2dev.sh
+++ utilities/switch2dev.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 #
-# a lil script to change the solr pipelines to point to dev instead of prod and to
-# send email notifications to jblowe and not the various "real" addresses.
+# a lil helper script to change the solr pipeline config to point to qa instead of prod and to
+# send email notifications to Dev jblowe and not the various "real" addresses.
+export DEVCONTACT="jblowe@berkeley.edu"
 cd
 perl -i -pe 's/prod-45.ist.berkeley.edu/dev-45.ist.berkeley.edu/' solrdatasources/*/*.sh
 perl -i -pe 's/=5313/=5114/' solrdatasources/*/*.sh
 perl -i -pe 's/=5307/=5117/' solrdatasources/*/*.sh
 perl -i -pe 's/=5310/=5119/' solrdatasources/*/*.sh
-perl -i -pe 's/cspace.support.lists./jblowe@/' solrdatasources/*/*.sh
-perl -i -pe 's/CONTACT=.*/CONTACT="jblowe\@berkeley.edu"/' solrdatasources/*/*.sh
+perl -i -pe 's/cspace.support.lists.berkeley.edu/$ENV{"DEVCONTACT"}/' solrdatasources/*/*.sh
+perl -i -pe 's/cspace.support.lists.berkeley.edu/$ENV{"DEVCONTACT"}/' ~/*.sh
+perl -i -pe 's/CONTACT=.*/CONTACT="$ENV{"DEVCONTACT"}"/' solrdatasources/*/*.sh
