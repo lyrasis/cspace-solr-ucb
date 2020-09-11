0d18fe4452be2a0c0444ffec250b9a41d3989ea3
John Lowe <jblowe@berkeley.edu>
2020-09-11 12:28:08 -0700

    CSW-239: use correct bash logic when deciding whether to zap solr core

--- common/post_to_solr.sh
+++ common/post_to_solr.sh
@@ -42,9 +42,12 @@ fi
 time python3 ../common/evaluate.py 4solr.${TENANT}.${FILE_PART}.csv /dev/null > ${TENANT}.counts.${FILE_PART}.csv &
 # zap the existing core, if the file we are loading is the CORE file.
 # (we might be loading several into this core)
-if [[ "$FILE_PART" -eq "${CORE}" ]]; then
+if ["$FILE_PART" == "${CORE}" ]; then
+  echo "POSTing 4solr.${TENANT}.${FILE_PART}.csv, ergo, we zap solr/${TENANT}-${CORE} first..."
   curl -S -s "http://localhost:8983/solr/${TENANT}-${CORE}/update" --data '<delete><query>*:*</query></delete>' -H 'Content-type:text/xml; charset=utf-8'
   curl -S -s "http://localhost:8983/solr/${TENANT}-${CORE}/update" --data '<commit/>' -H 'Content-type:text/xml; charset=utf-8'
+else
+  echo "POSTing 4solr.${TENANT}.${FILE_PART}.csv, i.e. adding documents to existing solr/${TENANT}-${CORE} ..."
 fi
 ##############################################################################
 # generate the field splitting parameters for the post to solr
