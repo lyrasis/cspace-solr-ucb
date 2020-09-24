2a65626a83309596d685c822c46d35380018a168
John Lowe <jblowe@berkeley.edu>
2020-09-23 19:30:11 -0700

    CSW-363: handle new pahma object status value in solr pipeline

--- pahma/part20.sql
+++ pahma/part20.sql
@@ -1,14 +1,13 @@
-SELECT cc.id, 'Deaccessioned' AS deaccessioned_s,
-STRING_AGG(DISTINCT REGEXP_REPLACE(osl.item, '^.*\)''(.*)''$', '\1'),'␥') AS "status_ss"
+SELECT cc.id,
+       CASE
+           WHEN STRING_AGG(DISTINCT REGEXP_REPLACE(osl.item, '^.*\)''(.*)''$', '\1'), '␥') ~ '(deaccessioned|transferred|repatriated|sold|exchanged|discarded|red-lined|destroyed)'
+           THEN
+               'Deaccessioned'
+           ELSE
+               ''
+           END                                                                    AS deaccessioned_s,
+       STRING_AGG(DISTINCT REGEXP_REPLACE(osl.item, '^.*\)''(.*)''$', '\1'), '␥') AS "status_ss"
 FROM collectionobjects_common cc
-LEFT OUTER JOIN collectionobjects_pahma_pahmaobjectstatuslist osl ON (cc.id=osl.id)
-JOIN misc m ON (m.id=cc.id AND m.lifecyclestate<>'deleted')
-WHERE osl.item = 'deaccessioned' 
-OR osl.item = 'transferred' 
-OR osl.item = 'repatriated' 
-OR osl.item = 'sold' 
-OR osl.item = 'exchanged' 
-OR osl.item = 'discarded' 
-OR osl.item = 'red-lined' 
-OR osl.item = 'destroyed' 
+         LEFT OUTER JOIN collectionobjects_pahma_pahmaobjectstatuslist osl ON (cc.id = osl.id)
+         JOIN misc m ON (m.id = cc.id AND m.lifecyclestate <> 'deleted')
 GROUP BY cc.id;
