75af0bf4800235fc10edf7f78ff297e5b737254e
John Lowe <jblowe@berkeley.edu>
2020-09-25 13:51:36 -0700

    CSW-363: for now, object status is not to be included in either PAHMA solr core

--- pahma/part20.sql
+++ pahma/part20.sql
@@ -6,7 +6,9 @@ SELECT cc.id,
            ELSE
                ''
            END                                                                    AS deaccessioned_s,
-       STRING_AGG(DISTINCT REGEXP_REPLACE(osl.item, '^.*\)''(.*)''$', '\1'), '␥') AS "status_ss"
+       ''                                                                         AS "status_ss"
+       -- object status is not being included in either public or internal portal, for now, ref. Michael Black
+       -- STRING_AGG(DISTINCT REGEXP_REPLACE(osl.item, '^.*\)''(.*)''$', '\1'), '␥') AS "status_ss"
 FROM collectionobjects_common cc
          LEFT OUTER JOIN collectionobjects_pahma_pahmaobjectstatuslist osl ON (cc.id = osl.id)
          JOIN misc m ON (m.id = cc.id AND m.lifecyclestate <> 'deleted')
