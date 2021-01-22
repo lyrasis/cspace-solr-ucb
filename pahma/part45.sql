3b57ba61c2de8d89cddb1ccd2d9c13c677d34676
John Lowe <jblowe@berkeley.edu>
2021-01-21 16:39:45 -0800

    CSW-386: add part45.sql, which pulls loan outs for the pahma solr cores

---
+++ pahma/part45.sql
@@ -0,0 +1,46 @@
+SELECT DISTINCT cc.id,
+                STRING_AGG(DISTINCT loc.loanoutnumber || ': ' ||
+                                    CASE
+                                        WHEN loc.borrower IS NULL OR loc.borrower = '' THEN 'unknown borrower'
+                                        ELSE REGEXP_REPLACE(loc.borrower, '^.*\)''(.*)''$', '\1') END ||
+                                    CASE
+                                        WHEN (loc.loanpurpose ILIKE '%research%' OR loc.loanpurpose ILIKE '%teaching%') AND
+                                             loc.borrowerscontact IS NOT NULL
+                                            THEN '/' || REGEXP_REPLACE(loc.borrowerscontact, '^.*\)''(.*)''$', '\1')
+                                        WHEN (loc.loanpurpose NOT ILIKE '%research%' AND
+                                              loc.loanpurpose NOT ILIKE '%teaching%') OR loc.loanpurpose IS NULL OR
+                                             loc.borrowerscontact IS NULL THEN '' END ||
+                                    CASE
+                                        WHEN sdg1.datedisplaydate IS NOT NULL AND sdg1.datedisplaydate <> '' AND
+                                             sdg2.datedisplaydate IS NOT NULL AND sdg2.datedisplaydate <> ''
+                                            THEN ' (' || sdg1.datedisplaydate || '–' || sdg2.datedisplaydate || ')'
+                                        WHEN sdg1.datedisplaydate IS NOT NULL AND sdg1.datedisplaydate <> '' AND
+                                             (sdg2.datedisplaydate IS NULL OR sdg2.datedisplaydate = '')
+                                            THEN ' (' || sdg1.datedisplaydate || '–unknown)'
+                                        WHEN (sdg1.datedisplaydate IS NULL OR sdg1.datedisplaydate = '') AND
+                                             sdg2.datedisplaydate IS NOT NULL AND sdg2.datedisplaydate <> ''
+                                            THEN ' (unknown–' || sdg2.datedisplaydate || ')'
+                                        WHEN (sdg1.datedisplaydate IS NULL OR sdg1.datedisplaydate = '') AND
+                                             (sdg2.datedisplaydate IS NULL OR sdg2.datedisplaydate = '')
+                                            THEN ' (dates unknown)' END, '␥')                          AS loan_info_ss,
+                STRING_AGG(DISTINCT loc.loanoutnumber, '␥')                                            AS loan_out_number_s,
+                STRING_AGG(DISTINCT REGEXP_REPLACE(loc.loanpurpose, '^.*\)''(.*)''$', '\1'), '␥')      AS purpose_ss,
+                STRING_AGG(DISTINCT REGEXP_REPLACE(loc.borrower, '^.*\)''(.*)''$', '\1'), '␥')         AS borrower_ss,
+                STRING_AGG(DISTINCT REGEXP_REPLACE(loc.borrowerscontact, '^.*\)''(.*)''$', '\1'),
+                           '␥')                                                                        AS borrower_contact_ss,
+                STRING_AGG(DISTINCT sdg1.datedisplaydate, '␥')                                         AS loanout_date_ss,
+                STRING_AGG(DISTINCT sdg2.datedisplaydate, '␥')                                         AS loan_return_date_ss
+
+FROM collectionobjects_common cc
+         JOIN hierarchy h1 ON (h1.id = cc.id)
+         JOIN relations_common rca ON (h1.name = rca.subjectcsid AND rca.objectdocumenttype = 'Loanout')
+         JOIN hierarchy hlo ON (hlo.name = rca.objectcsid)
+         JOIN loansout_common loc ON (hlo.id = loc.id)
+         JOIN misc mx ON (loc.id = mx.id AND mx.lifecyclestate = 'project')
+         LEFT OUTER JOIN hierarchy h3 ON (h3.parentid = loc.id AND h3.name = 'loansout_pahma:loanOutDateGroup')
+         LEFT OUTER JOIN structureddategroup sdg1 ON (h3.id = sdg1.id)
+         LEFT OUTER JOIN hierarchy h4 ON (h4.parentid = loc.id AND h4.name = 'loansout_pahma:loanReturnDateGroup')
+         LEFT OUTER JOIN structureddategroup sdg2 ON (h4.id = sdg2.id)
+WHERE loc.loanoutnumber NOT ILIKE '%Proposed%'
+  AND loc.loanoutnumber NOT ILIKE '%Cancelled%'
+GROUP BY cc.id;
