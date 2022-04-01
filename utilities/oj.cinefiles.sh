89d5d38cc92092a14de6fe0c4dfa1a495d57403f
John Lowe <jblowe@berkeley.edu>
2022-04-01 08:42:32 -0700

    CSW-629: put cinefiles denorm log output into regular solr pipeline file

--- utilities/oj.cinefiles.sh
+++ utilities/oj.cinefiles.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # run the 'legacy denormalizing' script first, to populate the 'cinefiles_denorm' db schema used by the portal
-cd ${HOME}/solrdatasources/cinefiles ; /bin/bash -l -c './cinefiles_denorm_nightly.sh' ; cd
+cd ${HOME}/solrdatasources/cinefiles ; /bin/bash -l -c './cinefiles_denorm_nightly.sh' | /usr/bin/ts '[%Y-%m-%d %H:%M:%S]' >> ${HOME}/logs/cinefiles.solr_extract_public.log  2>&1 ; cd
 # now run the regular pipeline that populates solr
 ${HOME}/solrdatasources/cinefiles/solrETL-public.sh        cinefiles 2>&1 | /usr/bin/ts '[%Y-%m-%d %H:%M:%S]' >> ${HOME}/logs/cinefiles.solr_extract_public.log  2>&1
