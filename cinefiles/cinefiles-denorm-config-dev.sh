7193d18eccefc6c94854f2c03794dc1ef50b515a
John Lowe <jblowe@berkeley.edu>
2022-04-28 17:13:53 -0700

    CSW-174: send solr pipeline emails generated by qa and dev servers to bit bucket

--- cinefiles/cinefiles-denorm-config-dev.sh
+++ cinefiles/cinefiles-denorm-config-dev.sh
@@ -1,3 +1,3 @@
 export CINEFILES_PGHOST="dba-postgres-dev-45.ist.berkeley.edu"
 export CINEFILES_PGPORT=5114
-export CINEFILES_CONTACT="jblowe@berkeley.edu"
+export CINEFILES_CONTACT="nobody@berkeley.edu"
--- pipeline-config-qa.sh
+++ pipeline-config-qa.sh
@@ -1,18 +1,21 @@
+
+# for qa and dev deployments, we send notification email to nobody
+
 export BAMPFA_SERVER="dba-postgres-dev-45.ist.berkeley.edu port=5113"
-export BAMPFA_CONTACT="jblowe@berkeley.edu"
+export BAMPFA_CONTACT="nobody@berkeley.edu"
 
 export BOTGARDEN_SERVER="dba-postgres-dev-45.ist.berkeley.edu port=5113"
-export BOTGARDEN_CONTACT="jblowe@berkeley.edu"
+export BOTGARDEN_CONTACT="nobody@berkeley.edu"
 
 export CINEFILES_SERVER="dba-postgres-dev-45.ist.berkeley.edu port=5113"
-export CINEFILES_CONTACT="jblowe@berkeley.edu"
+export CINEFILES_CONTACT="nobody@berkeley.edu"
 
 export PAHMA_SERVER="dba-postgres-dev-45.ist.berkeley.edu port=5107"
-export PAHMA_CONTACT="jblowe@berkeley.edu"
+export PAHMA_CONTACT="nobody@berkeley.edu"
 
 export UCJEPS_SERVER="dba-postgres-dev-45.ist.berkeley.edu port=5110"
-export UCJEPS_CONTACT="jblowe@berkeley.edu"
+export UCJEPS_CONTACT="nobody@berkeley.edu"
 
-export SUPPORT_CONTACT="jblowe@berkeley.edu"
+export SUPPORT_CONTACT="nobody@berkeley.edu"
 
 source ${HOME}/set_platform.sh
