5e31b1520ff3c7f22d360d900278286cb79f8e46
John Lowe <jblowe@berkeley.edu>
2022-05-01 09:47:05 -0700

    CSW-634: add -v option to set_platform; quiet otherwise

--- utilities/set_platform.sh
+++ utilities/set_platform.sh
@@ -1,20 +1,22 @@
 
-  if [[ ${USER} == 'app_cspace' ]]; then
-    echo
-    echo "USER is 'app_cspace', assuming deployment is on AWS/EC2"
-    echo
-    export SOLR_CACHE_DIR="/cspace/solr_cache"
-    export SOLR_LOG_DIR="/cspace/solr_logs"
-  elif [[ ${USER} == 'app_webapps' ]]; then
-    echo
-    echo "Assuming deployment is on RTL server"
-    echo
-    export SOLR_CACHE_DIR="/tmp"
-    export SOLR_LOG_DIR="${HOME}/logs"
-  else
-    echo
-    echo "Assuming deployment is local"
-    echo
-    export SOLR_CACHE_DIR="/tmp"
-    export SOLR_LOG_DIR="${HOME}/logs"
-  fi
+if [[ ${USER} == 'app_cspace' ]]; then
+  export PLATFORM="AWS/EC2"
+  export SOLR_CACHE_DIR="/cspace/solr_cache"
+  export SOLR_LOG_DIR="/cspace/solr_logs"
+elif [[ ${USER} == 'app_webapps' ]]; then
+  export PLATFORM="RTL server"
+  export SOLR_CACHE_DIR="/tmp"
+  export SOLR_LOG_DIR="${HOME}/logs"
+else
+  export PLATFORM="local"
+  export SOLR_CACHE_DIR="/tmp"
+  export SOLR_LOG_DIR="${HOME}/logs"
+fi
+if [[ "$1" == "-v" ]]; then
+  echo
+  echo "User is ${USER}"
+  echo "Assuming deployment is ${PLATFORM}"
+  echo "SOLR_CACHE_DIR: ${SOLR_CACHE_DIR}"
+  echo "SOLR_LOG_DIR: ${SOLR_LOG_DIR}"
+  echo
+fi
