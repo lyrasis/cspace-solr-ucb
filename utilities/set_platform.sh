9c91dd3da41f92f507f032abd2236c5703a60160
John Lowe <jblowe@berkeley.edu>
2022-05-04 15:58:29 -0700

    CSW-634: need to set a HOMEDIR (not HOME) var for some cron jobs

--- utilities/set_platform.sh
+++ utilities/set_platform.sh
@@ -1,14 +1,17 @@
 
 if [[ ${USER} == 'app_cspace' ]]; then
   export PLATFORM="AWS/EC2"
+  export HOMEDIR="/cspace"
   export SOLR_CACHE_DIR="/cspace/solr_cache"
   export SOLR_LOG_DIR="/cspace/solr_logs"
 elif [[ ${USER} == 'app_webapps' ]]; then
   export PLATFORM="RTL server"
+  export HOMEDIR=${HOME}
   export SOLR_CACHE_DIR="/tmp"
   export SOLR_LOG_DIR="${HOME}/logs"
 else
   export PLATFORM="local"
+  export HOMEDIR=${HOME}
   export SOLR_CACHE_DIR="/tmp"
   export SOLR_LOG_DIR="${HOME}/logs"
 fi
@@ -16,6 +19,7 @@ if [[ "$1" == "-v" ]]; then
   echo
   echo "User is ${USER}"
   echo "Assuming deployment is ${PLATFORM}"
+  echo "HOMEDIR: ${HOMEDIR}"
   echo "SOLR_CACHE_DIR: ${SOLR_CACHE_DIR}"
   echo "SOLR_LOG_DIR: ${SOLR_LOG_DIR}"
   echo
