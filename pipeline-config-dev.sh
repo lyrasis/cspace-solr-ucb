04831d0d9895eba00db97876dfe3c9813e0d4a73
John Lowe <jblowe@berkeley.edu>
2022-05-25 16:15:26 -0700

    CSW-634: use better dummy email addrs for dev and qa deployments

--- pipeline-config-dev.sh
+++ pipeline-config-dev.sh
@@ -1,21 +1,21 @@
 
 # for qa and dev deployments, we send notification email to nobody
 
-export BAMPFA_SERVER="dba-postgres-dev-45.ist.berkeley.edu port=5114"
-export BAMPFA_CONTACT="nobody@berkeley.edu"
+export BAMPFA_SERVER="dba-postgres-dev-45.ist.nowhere.edu port=5114"
+export BAMPFA_CONTACT="nobody@nowhere.edu"
 
-export BOTGARDEN_SERVER="dba-postgres-dev-45.ist.berkeley.edu port=5114"
-export BOTGARDEN_CONTACT="nobody@berkeley.edu"
+export BOTGARDEN_SERVER="dba-postgres-dev-45.ist.nowhere.edu port=5114"
+export BOTGARDEN_CONTACT="nobody@nowhere.edu"
 
-export CINEFILES_SERVER="dba-postgres-dev-45.ist.berkeley.edu port=5114"
-export CINEFILES_CONTACT="nobody@berkeley.edu"
+export CINEFILES_SERVER="dba-postgres-dev-45.ist.nowhere.edu port=5114"
+export CINEFILES_CONTACT="nobody@nowhere.edu"
 
-export PAHMA_SERVER="dba-postgres-dev-45.ist.berkeley.edu port=5117"
-export PAHMA_CONTACT="nobody@berkeley.edu"
+export PAHMA_SERVER="dba-postgres-dev-45.ist.nowhere.edu port=5117"
+export PAHMA_CONTACT="nobody@nowhere.edu"
 
-export UCJEPS_SERVER="dba-postgres-dev-45.ist.berkeley.edu port=5119"
-export UCJEPS_CONTACT="nobody@berkeley.edu"
+export UCJEPS_SERVER="dba-postgres-dev-45.ist.nowhere.edu port=5119"
+export UCJEPS_CONTACT="nobody@nowhere.edu"
 
-export SUPPORT_CONTACT="nobody@berkeley.edu"
+export SUPPORT_CONTACT="nobody@nowhere.edu"
 
 source ${HOME}/set_platform.sh
--- pipeline-config-qa.sh
+++ pipeline-config-qa.sh
@@ -1,21 +1,21 @@
 
 # for qa and dev deployments, we send notification email to nobody
 
-export BAMPFA_SERVER="dba-postgres-dev-45.ist.berkeley.edu port=5113"
-export BAMPFA_CONTACT="nobody@berkeley.edu"
+export BAMPFA_SERVER="dba-postgres-dev-45.ist.nowhere.edu port=5113"
+export BAMPFA_CONTACT="nobody@nowhere.edu"
 
-export BOTGARDEN_SERVER="dba-postgres-dev-45.ist.berkeley.edu port=5113"
-export BOTGARDEN_CONTACT="nobody@berkeley.edu"
+export BOTGARDEN_SERVER="dba-postgres-dev-45.ist.nowhere.edu port=5113"
+export BOTGARDEN_CONTACT="nobody@nowhere.edu"
 
-export CINEFILES_SERVER="dba-postgres-dev-45.ist.berkeley.edu port=5113"
-export CINEFILES_CONTACT="nobody@berkeley.edu"
+export CINEFILES_SERVER="dba-postgres-dev-45.ist.nowhere.edu port=5113"
+export CINEFILES_CONTACT="nobody@nowhere.edu"
 
-export PAHMA_SERVER="dba-postgres-dev-45.ist.berkeley.edu port=5107"
-export PAHMA_CONTACT="nobody@berkeley.edu"
+export PAHMA_SERVER="dba-postgres-dev-45.ist.nowhere.edu port=5107"
+export PAHMA_CONTACT="nobody@nowhere.edu"
 
-export UCJEPS_SERVER="dba-postgres-dev-45.ist.berkeley.edu port=5110"
-export UCJEPS_CONTACT="nobody@berkeley.edu"
+export UCJEPS_SERVER="dba-postgres-dev-45.ist.nowhere.edu port=5110"
+export UCJEPS_CONTACT="nobody@nowhere.edu"
 
-export SUPPORT_CONTACT="nobody@berkeley.edu"
+export SUPPORT_CONTACT="nobody@nowhere.edu"
 
 source ${HOME}/set_platform.sh
