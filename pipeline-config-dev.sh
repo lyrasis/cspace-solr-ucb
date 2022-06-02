c378603458a69a6c4aab046c59a14f426fc907ac
John Lowe <jblowe@berkeley.edu>
2022-06-02 16:15:45 -0700

    CSW-634: backout incorrect database hostname in dev and qa for solr pipelines

--- pipeline-config-dev.sh
+++ pipeline-config-dev.sh
@@ -2,19 +2,19 @@
 
 # for qa and dev deployments, we send notification email to nobody
 
-export BAMPFA_SERVER="dba-postgres-dev-45.ist.nowhere.edu port=5114"
+export BAMPFA_SERVER="dba-postgres-dev-45.ist.berkeley.edu port=5114"
 export BAMPFA_CONTACT="nobody@nowhere.edu"
 
-export BOTGARDEN_SERVER="dba-postgres-dev-45.ist.nowhere.edu port=5114"
+export BOTGARDEN_SERVER="dba-postgres-dev-45.ist.berkeley.edu port=5114"
 export BOTGARDEN_CONTACT="nobody@nowhere.edu"
 
-export CINEFILES_SERVER="dba-postgres-dev-45.ist.nowhere.edu port=5114"
+export CINEFILES_SERVER="dba-postgres-dev-45.ist.berkeley.edu port=5114"
 export CINEFILES_CONTACT="nobody@nowhere.edu"
 
-export PAHMA_SERVER="dba-postgres-dev-45.ist.nowhere.edu port=5117"
+export PAHMA_SERVER="dba-postgres-dev-45.ist.berkeley.edu port=5117"
 export PAHMA_CONTACT="nobody@nowhere.edu"
 
-export UCJEPS_SERVER="dba-postgres-dev-45.ist.nowhere.edu port=5119"
+export UCJEPS_SERVER="dba-postgres-dev-45.ist.berkeley.edu port=5119"
 export UCJEPS_CONTACT="nobody@nowhere.edu"
 
 export SUPPORT_CONTACT="nobody@nowhere.edu"
--- pipeline-config-qa.sh
+++ pipeline-config-qa.sh
@@ -2,19 +2,19 @@
 
 # for qa and dev deployments, we send notification email to nobody
 
-export BAMPFA_SERVER="dba-postgres-dev-45.ist.nowhere.edu port=5113"
+export BAMPFA_SERVER="dba-postgres-dev-45.ist.berkeley.edu port=5113"
 export BAMPFA_CONTACT="nobody@nowhere.edu"
 
-export BOTGARDEN_SERVER="dba-postgres-dev-45.ist.nowhere.edu port=5113"
+export BOTGARDEN_SERVER="dba-postgres-dev-45.ist.berkeley.edu port=5113"
 export BOTGARDEN_CONTACT="nobody@nowhere.edu"
 
-export CINEFILES_SERVER="dba-postgres-dev-45.ist.nowhere.edu port=5113"
+export CINEFILES_SERVER="dba-postgres-dev-45.ist.berkeley.edu port=5113"
 export CINEFILES_CONTACT="nobody@nowhere.edu"
 
-export PAHMA_SERVER="dba-postgres-dev-45.ist.nowhere.edu port=5107"
+export PAHMA_SERVER="dba-postgres-dev-45.ist.berkeley.edu port=5107"
 export PAHMA_CONTACT="nobody@nowhere.edu"
 
-export UCJEPS_SERVER="dba-postgres-dev-45.ist.nowhere.edu port=5110"
+export UCJEPS_SERVER="dba-postgres-dev-45.ist.berkeley.edu port=5110"
 export UCJEPS_CONTACT="nobody@nowhere.edu"
 
 export SUPPORT_CONTACT="nobody@nowhere.edu"
